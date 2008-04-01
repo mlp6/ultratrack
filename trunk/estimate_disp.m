d0ba1ccc833108855c0b672d68b2ba8cc1f127a8
mlp6 <mlp6@57c3da9d-1720-0410-8437-ae526f62eb58>
2008-04-01 16:01:36 +0000

    testing phase for new sub-functions
    git-svn-id: https://hogwarts.egr.duke.edu/svn/ultratrack@11 57c3da9d-1720-0410-8437-ae526f62eb58

---
+++ trunk/estimate_disp.m
@@ -0,0 +1,47 @@
+function [D,C]=estimate_disp(rfdata,alg,kernelsize)
+% function [D,C]=estimate_disp(rfdata,alg,kernelsize)
+% 
+% SUMMARY: Estimate displacement from the tracking simulations using a variety
+% of tracking algorithms.
+%
+% INPUTS:
+%   rfdata (float) - matrix of RF data [axial x lat x t]
+%   alg (string) - tracking algorithm 
+%       'samtrack' - Steve McAleavey's cross correlator
+%       'samauto' - Steve McAleavey's Kasai algorithm (auto correlator)
+%       'ncorr' - Gianmarco's cross correlator
+%       'loupas' - Gianmarco's Loupas algorithm (auto correlator)
+%   kernelsize (int) - number of samples in the tracking kernel
+%
+% OUTPUTS:
+%   D (float) - displacement estimates (um)
+%   C (float) - correlation coefficients (normalized)
+%
+% EXAMPLE:
+%
+% Mark 03/31/08
+
+switch alg
+    case 'samtrack',
+        for n=1:size(rfdata,2)
+        % MODIFIED THE CODE TO HAVE A VARIABLE KERNEL
+        % SIZE AS A FUNCTION OF FREQUENCY TO MAINTAIN A
+        % CONSTANT 2.5 CYCLES / KERNEL
+        % MARK 01/24/05
+        %[D(:,:,n),C(:,:,n)]=sam_track(squeeze(bigRF(:,n,:)),35,-5,5);
+                    %
+                    % Allow for variable kernel sizes
+                    %
+        %[D(:,:,n),C(:,:,n)]=sam_track(squeeze(bigRF(:,n,:)),35*7/Freq,-5,5);
+        [D(:,:,n),C(:,:,n)]=sam_track(squeeze(rfdata(:,n,:)),KernelSize*7/Freq,-5,5);
+        end;
+
+    case 'samauto',
+        error('samauto tracking has not been intengrated yet');
+    case 'ncorr',
+        error('ncorr tracking has not been intengrated yet');
+    case 'loupas',
+        error('loupas tracking has not been intengrated yet');
+    otherwise,
+        error(sprintf('%s cannot be found as a tracking algorithm',TRACKPARAMS.TRACK));
+end;
--- trunk/read_dot_dyn.m
+++ trunk/read_dot_dyn.m
@@ -2,22 +2,23 @@ function [node_loc,X,Y,Z] = read_dot_dyn(fname);
 
 % function [nodes,X,Y,Z] = read_dot_dyn(fname);                     
 %
-% UPDATE THIS DESCRIPTION!!  
+% read_dot_dyn reads a .dyn file that contain information about node IDs and
+% their x,y,z locations and returns 4 3D matricies nodes is matrix of node
+% numbers
 %
-% read_dot_dyn reads a .dyn file and returns 4 3D matricies
-% nodes is matrix of node numbers
 % X,Y,Z are matricies of X, Y, and Z node locations (initial)
 %
 % These matricies are part of the input to scat_and_disp.m
 %
+% fname is the name of your .dyn file that has comma-delimited node information
 %
-% fname is the name of your .dyn file
-%
-% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Builds upon read_dyna_nodes.m
 % Last modified 10/18/04 Stephen McAleavey, U. Rochester BME
-%
- 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Modified to use comma delimiters, not spaces.
+% Mark 04/01/08
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 endofline=sprintf('\n');
   
@@ -29,8 +30,6 @@ if (fid == -1),
 	disp(['Can''t open ' fname]);
 	return;
 	end;
-
-
  
 % find last word just before data... 
 s=fscanf(fid,'%s',1);
@@ -44,7 +43,7 @@ while(c~=endofline);
 	c=fscanf(fid,'%c',1);
 	end;
 % Suck in data...  
-[node_loc,count]=fscanf(fid,'%d %f %f %f',[4,inf]);
+[node_loc,count]=fscanf(fid,'%d,%f,%f,%f',[4,inf]);
 node_loc=node_loc';
 fclose(fid);
 
@@ -75,7 +74,6 @@ node_loc=node_loc(I,:);
 node_loc=n2;
 
 % figure out dimensions
-
 zsum=sum(n2(:,4)==n2(1,4));
 zdim=size(n2,1)/zsum;
 ysum=sum(n2(1:zsum,3)==n2(1,3));
@@ -84,11 +82,7 @@ xsum=sum(n2(1:ysum,2)==n2(1,2));
 xdim=ysum/xsum;
                                                                           
 % create outputs by reshaping node_loc
-
 X=reshape(node_loc(:,2),xdim,ydim,zdim);
 Y=reshape(node_loc(:,3),xdim,ydim,zdim);
 Z=reshape(node_loc(:,4),xdim,ydim,zdim);
 node_loc=reshape(node_loc(:,1),xdim,ydim,zdim);
-
-
-
