88459a3454807d380476097102b34c2929eaa159
mlp6 <mlp6@57c3da9d-1720-0410-8437-ae526f62eb58>
2008-04-01 20:35:44 +0000

    Removed the probe definitions that had different center frequencies - those shouldn't be used anymore.  Also renamed arfi_scans.m to arfi_scans_template.m to avoid name conflicts with local CWD copies.
    git-svn-id: https://hogwarts.egr.duke.edu/svn/ultratrack@12 57c3da9d-1720-0410-8437-ae526f62eb58

--- trunk/estimate_disp.m
+++ trunk/estimate_disp.m
@@ -33,7 +33,7 @@ switch alg
                     % Allow for variable kernel sizes
                     %
         %[D(:,:,n),C(:,:,n)]=sam_track(squeeze(bigRF(:,n,:)),35*7/Freq,-5,5);
-        [D(:,:,n),C(:,:,n)]=sam_track(squeeze(rfdata(:,n,:)),KernelSize*7/Freq,-5,5);
+        [D(:,:,n),C(:,:,n)]=sam_track(squeeze(rfdata(:,n,:)),kernelsize*7/Freq,-5,5);
         end;
 
     case 'samauto',
