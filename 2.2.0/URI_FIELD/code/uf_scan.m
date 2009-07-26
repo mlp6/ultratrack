6113a7c76e2748abcc88625208872788458d898c
mlp6 <mlp6@57c3da9d-1720-0410-8437-ae526f62eb58>
2009-07-26 18:15:54 +0000

    (1) Finished re-organizing the directory structure to be consistent w/ Ned's processing code.
    (2) 2.2.0 is the latest version - its highlights include:
        [a] cc_coeff -> cc_coef in the displacement estimation function to be consistent w/ Ned's code
        [b] a "placeholder" reference timestep has been added to cc_coef to have the same dimensions as arfidata
     
    TO DO:
    * Transition to Ned's xcorr code once it has been validated.
    
    
    git-svn-id: https://hogwarts.egr.duke.edu/svn/ultratrack@28 57c3da9d-1720-0410-8437-ae526f62eb58

---
+++ 2.2.0/URI_FIELD/code/uf_scan.m
@@ -0,0 +1,46 @@
+function [rf,t0]=uf_scan(probe,beamset,tx,rx,phantom,txoffset,varargin);
+% Added txoffset as an input to allow for the Tx beam to be
+% offset (m) from the Rx beam; this is passed to uf_set_beam()
+% Mark 06/17/05
+
+% Change to accomodate an additional input variable
+% Mark 06/16/06
+%if (nargin>5),
+if (nargin>6),
+	stat_win=(varargin{1}==2);
+	stat_txt=(varargin{1}==1);
+	else
+	stat_txt=1;
+	stat_win=0;
+	end;
+	
+
+% Select each vector in the beamset and calculate echo signal
+%
+rfdata=zeros(1,beamset.no_beams);
+for n_vector=1:beamset.no_beams,
+	if stat_txt,
+		disp(sprintf('Processing Vector %d of %d',n_vector, beamset.no_beams));
+		end;
+	
+	% Modified the code to pass txoffset to uf_set_beam()
+	% Mark 06/16/05
+	%uf_set_beam(tx,rx,probe,beamset,1,n_vector);
+	uf_set_beam(tx,rx,probe,beamset,1,n_vector,txoffset);
+
+	[v,t1]=calc_scat(tx,rx,phantom.position,phantom.amplitude);
+	if (size(rfdata,1)<length(v)),
+		disp('Memory');
+		rfdata=[rfdata ;zeros(length(v)-size(rfdata,1),beamset.no_beams) ];
+		end;
+	rfdata(1:length(v),n_vector)=v;
+	start_times(n_vector)=t1;
+end;
+% Create rf with equal t0s from rfdata
+%
+[rf,t0]=uf_time_eq(rfdata,start_times,probe.field_sample_freq);
+t0=t0-length(probe.impulse_response)*2/probe.field_sample_freq;
+
+
+
+
