0ad27e6f50e65cc4791dcad1da8409c38a4583de
mlp6 <mlp6@57c3da9d-1720-0410-8437-ae526f62eb58>
2010-10-26 02:24:33 +0000

    * Modified CalcSpeckleSNR.m to now be a function to accomodate variable inputs. * Updated convert_disp_dat to work w/ the latest FE sims (disp.dat files).
    git-svn-id: https://hogwarts.egr.duke.edu/svn/ultratrack@32 57c3da9d-1720-0410-8437-ae526f62eb58

--- 2.4.0/CalcSpeckleSNR.m
+++ 2.4.0/CalcSpeckleSNR.m
@@ -1,15 +1,31 @@
 function [envSNR]=CalcSpeckleSNR
-% function [envSNR]=CalcSpeckleSNR
-% calculate the speckle SNR for our typicaly VF10-5
-% configuration (foc 20 mm, F/1, 7 MHz)
+% function [envSNR]=CalcSpeckleSNR(rfmat,axmin,axmax)
 %
-% Mark 01/07/05
+% Calculate the speckle SNR from the sim RF data
+% INPUTS:
+%   rfmat (string) - rf sim filename
+%   axmin (int) - min axial index to compute SNR over
+%   axmax (int) - max axial index to computer SNR over
+%
+% OUTPUT:
+%   envSNR (float) - speckle SNR
+%
+% EXAMPLE: [envSNR] = CalcSpeckleSNR('rf001.mat',2482,2715);
+%
+% Mark 2010-10-25
 
-load rf001.mat
-env = abs(hilbert(rf(2482:2715,:)));
+disp(sprintf('Loading %s . . . ', rfmat));
+load(rfmat);
+
+% compute the envelope for the RF data over the specified axial range
+env = abs(hilbert(rf(axmin:axmax,:)));
+
+% get some averages over all of the lateral lines in the RF data
 for i=1:size(rf,2),
 	envmean(i) = mean(env(:,i));
 	envstd(i) = std(env(:,i));
 	envSNRa(i) = envmean(i)/envstd(i);
 end;
+
+% output the average SNR
 envSNR=mean(envSNRa);
