f437457395d78c241e6187da1459be3cda60ed07
Mark Palmeri <mlp6@duke.edu>
2013-08-28 19:03:04 -0400

    Updated aspects of the cluster option to generate RF data to explicitely add data paths, change the qsub syntax and preserve the generated *.sge files for post-run review.  Would like to make the qsub syntax be a driver script option.

--- gen_cluster_sge.m
+++ gen_cluster_sge.m
@@ -1,4 +1,4 @@
-function sgefilename = gen_cluster_sge(Function_Name,Scratch_Dir,Max_Index,UniqueID,Memory_Limit)
+function sgefilename = gen_cluster_sge(Function_Name,Scratch_Dir,Ultratrack_Path,Max_Index,UniqueID,Memory_Limit)
 
 fid = fopen('cluster_template.sge','rt');
 s = fread(fid);
@@ -13,7 +13,8 @@ s1 = strrep(char(s'),'MAX_INDEX',num2str(Max_Index));
 s1 = strrep(s1,'MEMORY_LIMIT',num2str(Memory_Limit));
 s1 = strrep(s1,'UNIQUEID',UniqueID);
 s1 = strrep(s1,'FUNCTION_NAME',Function_Name);
-s1 = strrep(s1,'SCRATCHDIR',Scratch_Dir);
+%s1 = strrep(s1,'SCRATCHDIR',Scratch_Dir);
+s1 = strrep(s1,'ULTRATRACK',Ultratrack_Path);
 
 s2 = double(s1)';
 sgefilename = sprintf('%s_SGE.sge',UniqueID);
