4877783d59230f9ee7946d5412fb28968a860548
Mark Palmeri <mlp6@duke.edu>
2014-01-04 12:21:14 -0500

    define impulseResponse as the quadrature component of gauspuls to make sure that it is zero-mean

--- makeImpulseResponse.m
+++ makeImpulseResponse.m
@@ -8,4 +8,9 @@ function [impulseResponse t] = makeImpulseResponse(fractionalBandwidth, centerFr
 
 tc = gauspuls('cutoff',centerFreq,fractionalBandwidth,-6,-40);
 t = -tc:1/Fs:tc;
-impulseResponse =gauspuls(t,centerFreq,fractionalBandwidth);
+% I am concerened that using YI might introduce low-frequency errors in to the
+% RF data since it does not enforce a zero-mean constraint; but the quadrature
+% component does, so I will try defining the impulse response using Q instead
+% of I to see if that makes a difference
+[YI,YQ] = gauspuls(t,centerFreq,fractionalBandwidth);
+impulseResponse = YQ;
