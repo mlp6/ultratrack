fa45d5f90e70c9fa5a0356ba9f08a21c7c4e057e
Mark Palmeri <mlp6@duke.edu>
2014-08-06 13:14:31 -0400

    added UNC driver and removed broken rev lat phantom gen

---
+++ ultratrack_driver.m
@@ -0,0 +1,294 @@
+function []=ultratrack_driver(varargin)
+% function []=bme790_ultratrack_driver(phantom_seed)
+% INPUTS:
+%   phantom_seed (int) - scatterer position RNG seed
+%   DEST_DIR (string) - optional path to save location (default is pwd)
+% OUTPUTS:
+%   Nothing returned, but lots of files and directories created in PATH
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% this is branched off of v2.6.1 to work for Kathy's BME790 S13 class
+% URI toolbox calls removed
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  --------------PARSE INPUTS---------------------------------------
+if nargin > 2
+    phantom_seed = varargin{1};
+    delta = varargin{2};
+    DEST_DIR = varargin{3};
+elseif nargin == 2
+    phantom_seed = varargin{1};
+    delta = varargin{2};
+    DEST_DIR = pwd
+elseif nargin == 1
+    phantom_seed = varargin{1};
+    delta = [0 0 0];
+    DEST_DIR = pwd;
+else
+    phantom_seed = 0;
+    delta = [0 0 0];
+    DEST_DIR = pwd;
+end
+
+%%  --------------IMAGING PARAMETERS---------------------------------
+PARAMS.PROBE ='vf7-3';
+PARAMS.COMPUTATIONMETHOD = 'parfor'; % 'cluster','parfor', or 'none'
+
+probe=uf_txt_to_probe([PARAMS.PROBE '.txt']);
+
+% setup some Field II parameters
+PARAMS.field_sample_freq = 200e6; % Hz
+PARAMS.c = 1540; % sound speed (m/s)
+
+% TRANSMIT BEAM LOCATIONS
+PARAMS.XMIN =   0;	   % Leftmost scan line (m)
+PARAMS.XSTEP =  0;      % Azimuth step size (m);
+PARAMS.XMAX =   0;	   % Rightmost scan line (m)
+PARAMS.THMIN =  0;      % Leftmost azimuth angle (deg)
+PARAMS.THSTEP = 0;    % Azimuth angle step(deg)
+PARAMS.THMAX =  0;      % Rightmost azimuth angle (deg)
+PARAMS.PHIMIN = 0;      % Frontmost elevation angle (deg)
+PARAMS.PHISTEP = 0;    % Elevation angle step(deg)
+PARAMS.PHIMAX = 0;      % Backmost elevation angle (deg)
+PARAMS.YMIN =  0;		% Frontmost scan line (m)
+PARAMS.YSTEP = 0;      % Elevation step size (m);
+PARAMS.YMAX =  0;	    % Backmost scan line (m)
+PARAMS.APEX = 0; %Apex of scan geometry. Set to 0 for linear scanning.
+PARAMS.TX_FOCUS= 20e-3;    % Transmit focus depth (m)
+PARAMS.TX_F_NUM=[1.5 1.5];      % Transmit f number (the "y" number only used for 2D matrix arrays)
+PARAMS.TX_FREQ=6.15e6;      % Transmit frequency (Hz)
+PARAMS.TX_NUM_CYCLES=2;     % Number of cycles in transmit toneburst
+PARAMS.TX_APOD_TYPE=1;      %Transmit apodization (0 = none, 1 = hamming)
+PARAMS.RX_FOCUS= 0;          % Depth of receive focus - use 0 for dyn. foc
+PARAMS.RX_F_NUM=[0.75 0.75];  % Receive aperture f number (the "y" number only used for 2D matrix arrays)
+PARAMS.RX_GROW_APERTURE=1;  %Receive aperture growth (if true, constant F/# through depth)
+PARAMS.RX_APOD_TYPE=1;      %Receive apodization (0 = none, 1 = hamming)
+PARAMS.MINDB = -40; %minimum contribution for including a scatter in sensitivity-based reduction
+PARAMS.GRIDSPACING = [0.5e-3 0.5e-3 0.5e-3]; %Spacing of grid for sensitivity-based scatterer reduction (m)
+% lateral offset of the Tx beam from the Rx beam (m) to simulated prll rx
+% tracking (now a 2 element array, v2.6.0) (m)
+%%% 2012.11.2 pjh7 adding native parallel receive functionality at lower
+%%% level and swapped tx offsets for rx offsets
+PARAMS.NO_PARALLEL = [1 1]; %[no_X no_Y]
+PARAMS.PARALLEL_SPACING = [1 1]; % Spread || RX Beams Multiplier [X Y]
+
+%% -------------- DISPLACEMENT TRACKING PARAMETERS --------------
+% 'samtrack','samauto','ncorr','loupas'
+TRACKPARAMS.TRACK_ALG='ncorr';
+TRACKPARAMS.WAVELENGTHS = 1.5; %Size of tracking kernel in wavelengths
+TRACKPARAMS.MAX_SEARCH = 30e-6; %Size of search window in meters (defines the largest trackable displacement)
+TRACKPARAMS.PRE_INTERP_FACTOR = 1; %RF interpolation factor applied prior to displacement estimator
+TRACKPARAMS.CORR_SPARSENESS = 5; %Axial (fast-time) downsample factor (use this to reduce the data set and speed up total processing time)
+
+
+%% ------------------PHANTOM PARAMETERS----------------------------
+% file containing comma-delimited node data
+DYN_FILE='nodes.dyn'; % <- CHANGE THIS TO YOUR SIMS'S nodes.dyn FILE
+DEST_DIR = fullfile(DEST_DIR);
+ZDISPFILE = fullfile(DEST_DIR,'disp.dat');
+
+% Read the .dyn file to get node positions
+[~,X,Y,Z]=read_dot_dyn(DYN_FILE);
+
+% setup phantom parameters (PPARAMS)
+PPARAMS.SYM = 'q'; %symmetry condition ('q' = quarter, 'h' = half, 'n' = none)
+
+% WARNING: DO NOT EXCEED YOUR MESH DIMENSIONS!!!!
+PPARAMS.xmin=min(X(:)); 
+PPARAMS.xmax=max(X(:)); 
+PPARAMS.ymin=min(Y(:));  
+PPARAMS.ymax=max(Y(:));  
+PPARAMS.zmin= min(Z(:));
+PPARAMS.zmax=-0.1;%max(Z(:));  
+
+%Account for symmetry condition with the min and max coordinates (these are
+%used later to determine the bounds of the ultrasound phantom)
+if strcmp(PPARAMS.SYM,'q')
+    PPARAMS.xmax = -(PPARAMS.xmin);
+    PPARAMS.ymin = -(PPARAMS.ymax);
+end
+
+PPARAMS.TIMESTEP=[];	% Timesteps to simulate.  Leave empty to simulate all timesteps.
+% ^^^^ WARNING: IF SPECIFYING TIMESTEPS, THEN YOUR ARRAY MUST CONTAIN ONLY CONSECUTIVE INTEGERS; IT CANNOT BE SPARSE
+
+% compute number of scatterers to use
+NUM_SCAT_PER_RES_CELL = 11;
+lambda = PARAMS.c/PARAMS.TX_FREQ; %m
+
+%Calculate rough estimate of resolution cell size (lambda*F#)
+RES_CELL_LAT = lambda*mean([PARAMS.RX_F_NUM(1) PARAMS.TX_F_NUM(1)])*1e2; %cm. Take the mean F/# used between TX and RX 
+RES_CELL_ELE = lambda*mean([PARAMS.RX_F_NUM(2) PARAMS.TX_F_NUM(2)])*1e2; %cm.
+RES_CELL_AX  = lambda*1e2; %cm
+
+SCATTERER_DENSITY = NUM_SCAT_PER_RES_CELL/(RES_CELL_LAT*RES_CELL_ELE*RES_CELL_AX); % scatterers/cm^3  
+TRACKING_VOLUME = (PPARAMS.xmax-PPARAMS.xmin)*(PPARAMS.ymax-PPARAMS.ymin)*(PPARAMS.zmax-PPARAMS.zmin); % cm^3
+
+%Compute number of scatterers
+PPARAMS.N = round(SCATTERER_DENSITY * TRACKING_VOLUME); % number of scatterers to randomly distribute over the tracking volume
+
+PPARAMS.seed=phantom_seed;         % RNG seed
+
+%Optional point-scatterer locations
+USE_POINT_SCATTERERS = 0;
+if USE_POINT_SCATTERERS
+    PPARAMS.pointscatterers.x = 1e-3*[-30:4:30]; % X locations of point scatterers
+    PPARAMS.pointscatterers.z = 1e-3*[2:4:45]; % Y locations of point scatterers
+    PPARAMS.pointscatterers.y = 1e-3*[0]; % Z locations of point scatterers
+    PPARAMS.pointscatterers.a = 1.0; % Point scatterer amplitude
+end
+
+PPARAMS.delta=delta;   % rigid pre-zdisp-displacement scatterer translation,
+% in the dyna coordinate/unit system to simulate s/w
+% sequences - (2012.11.15 This may be outdated. Peter)
+
+%% ---------- AUTOMATICALLY CALCULATED PARAMETERS -------------------
+% You shouldn't need to change these unless you are using one of the
+% override functions to specify non-uniform TX or || RX beam spacings
+
+% ---------------- TRANSMIT OVERRIDE ---------------------------
+% Override only if necessary. If you specify your own origins and angles,
+% you can set the origins and angles as such:
+%   ORIGIN  ANGLE
+%   [1x1]   [1x1]   - single TX Beam
+%   [1xN]   [1xN]   - specifiy origin and angle for each beam
+%   [1xN]   [1x1]   - specify each origin, use same angle
+%   [1x1]   [1xN]   - specify each angle, use same origin
+
+TX_BEAM_OVERRIDE = 0;
+
+if TX_BEAM_OVERRIDE
+    PARAMS.BEAM_ORIGIN_X = 0;
+    PARAMS.BEAM_ORIGIN_Y = 0;
+    PARAMS.BEAM_ANGLE_X = 0;
+    PARAMS.BEAM_ANGLE_Y = 0;
+else
+    
+    PARAMS.BEAM_ORIGIN_X = PARAMS.XMIN:PARAMS.XSTEP:PARAMS.XMAX;
+    PARAMS.BEAM_ORIGIN_Y = PARAMS.YMIN:PARAMS.YSTEP:PARAMS.YMAX;
+    PARAMS.BEAM_ANGLE_X = PARAMS.THMIN:PARAMS.THSTEP:PARAMS.THMAX;
+    PARAMS.BEAM_ANGLE_Y = PARAMS.PHIMIN:PARAMS.PHISTEP:PARAMS.PHIMAX;
+    
+    if PARAMS.XSTEP == 0;
+        PARAMS.BEAM_ORIGIN_X = (PARAMS.XMIN + PARAMS.XMAX)/2;
+    end
+    if PARAMS.YSTEP == 0;
+        PARAMS.BEAM_ORIGIN_Y = (PARAMS.YMIN + PARAMS.YMAX)/2;
+    end
+    if PARAMS.THSTEP == 0;
+        PARAMS.BEAM_ANGLE_X = (PARAMS.THMIN + PARAMS.THMAX)/2;
+    end
+    if PARAMS.PHISTEP == 0;
+        PARAMS.BEAM_ANGLE_Y = (PARAMS.PHIMIN + PARAMS.PHIMAX)/2;
+    end
+end
+
+PARAMS.BEAM_ANGLE_X = deg2rad(PARAMS.BEAM_ANGLE_X);
+PARAMS.BEAM_ANGLE_Y = deg2rad(PARAMS.BEAM_ANGLE_Y);
+
+
+if length(PARAMS.BEAM_ANGLE_X) > 1 && length(PARAMS.BEAM_ORIGIN_X) > 1 && length(PARAMS.BEAM_ORIGIN_X)~=length(PARAMS.BEAM_ANGLE_X);
+    error('BEAM_ORIGIN_X and BEAM_ANGLE_X cannot both be vectors and have different lengths.')
+end
+if length(PARAMS.BEAM_ANGLE_Y) > 1 && length(PARAMS.BEAM_ORIGIN_Y) > 1 && length(PARAMS.BEAM_ORIGIN_Y)~=length(PARAMS.BEAM_ANGLE_Y);
+    error('BEAM_ORIGIN_Y and BEAM_ANGLE_Y cannot both be vectors and have different lengths.')
+end
+
+PARAMS.NO_BEAMS_X = max(length(PARAMS.BEAM_ORIGIN_X),length(PARAMS.BEAM_ANGLE_X));
+PARAMS.NO_BEAMS_Y = max(length(PARAMS.BEAM_ORIGIN_Y),length(PARAMS.BEAM_ANGLE_Y));
+
+
+%  ----------------- PARALLEL RX OVERRIDE ---------------------------
+% Override if necessary. Default spaces || RX evenly between TX beams. If
+% only a single TX beam is used, the step size dictates the spread of the
+% || RX beams (Peter, 2012.11.15)
+
+%FULL_PARLLEL_OVERRIDE lets you specify the parallel receive matrix. Each
+%row corresponds to a parallel beam. the columns are x-offset (m),
+%y-offset(m), x-angle offset (deg) y-angle offset(deg), respectively.
+PARALLEL_OVERRIDE = 0;
+
+if PARALLEL_OVERRIDE
+    PARAMS.RXOFFSET = [0 0 0 0];
+    PARAMS.RXOFFSET(:,3:4) = deg2rad(PARAMS.RXOFFSET(:,3:4));
+else
+    PARALLEL_TH_MIN  = -0.5*PARAMS.THSTEP*PARAMS.PARALLEL_SPACING(1)* (PARAMS.NO_PARALLEL(1)-1)/(PARAMS.NO_PARALLEL(1));
+    PARALLEL_TH_MAX  =  0.5*PARAMS.THSTEP*PARAMS.PARALLEL_SPACING(1)* (PARAMS.NO_PARALLEL(1)-1)/(PARAMS.NO_PARALLEL(1));
+    PARALLEL_PHI_MIN = -0.5*PARAMS.PHISTEP*PARAMS.PARALLEL_SPACING(2)*(PARAMS.NO_PARALLEL(2)-1)/(PARAMS.NO_PARALLEL(2));
+    PARALLEL_PHI_MAX =  0.5*PARAMS.PHISTEP*PARAMS.PARALLEL_SPACING(2)*(PARAMS.NO_PARALLEL(2)-1)/(PARAMS.NO_PARALLEL(2));
+    PARALLEL_X_MIN   = -0.5*PARAMS.XSTEP*PARAMS.PARALLEL_SPACING(1)*  (PARAMS.NO_PARALLEL(1)-1)/(PARAMS.NO_PARALLEL(1));
+    PARALLEL_X_MAX   =  0.5*PARAMS.XSTEP*PARAMS.PARALLEL_SPACING(1)*  (PARAMS.NO_PARALLEL(1)-1)/(PARAMS.NO_PARALLEL(1));
+    PARALLEL_Y_MIN   = -0.5*PARAMS.YSTEP*PARAMS.PARALLEL_SPACING(2)*  (PARAMS.NO_PARALLEL(2)-1)/(PARAMS.NO_PARALLEL(2));
+    PARALLEL_Y_MAX   =  0.5*PARAMS.YSTEP*PARAMS.PARALLEL_SPACING(2)*  (PARAMS.NO_PARALLEL(2)-1)/(PARAMS.NO_PARALLEL(2));
+    
+    PARALLEL_TH_OFFSET0 = deg2rad(linspace(PARALLEL_TH_MIN,PARALLEL_TH_MAX,PARAMS.NO_PARALLEL(1)));    %RADIANS!
+    PARALLEL_PHI_OFFSET0 = deg2rad(linspace(PARALLEL_PHI_MIN,PARALLEL_PHI_MAX,PARAMS.NO_PARALLEL(2))); %RADIANS!
+    PARALLEL_X_OFFSET0 = linspace(PARALLEL_X_MIN,PARALLEL_X_MAX,PARAMS.NO_PARALLEL(1));
+    PARALLEL_Y_OFFSET0 = linspace(PARALLEL_Y_MIN,PARALLEL_Y_MAX,PARAMS.NO_PARALLEL(2));
+    
+    [PARALLEL_TH_OFFSET PARALLEL_PHI_OFFSET] = meshgrid(PARALLEL_TH_OFFSET0,PARALLEL_PHI_OFFSET0);
+    [PARALLEL_X_OFFSET PARALLEL_Y_OFFSET] = meshgrid(PARALLEL_X_OFFSET0,PARALLEL_Y_OFFSET0);
+    
+    PARAMS.RXOFFSET =  [PARALLEL_X_OFFSET(:) PARALLEL_Y_OFFSET(:) PARALLEL_TH_OFFSET(:) PARALLEL_PHI_OFFSET(:)];
+end
+
+%% ---------------  MAP DYNA DISPLACEMENTS TO SCATTERER FIELD ------------
+P=rmfield(PPARAMS,{'TIMESTEP'});
+if isfield(PPARAMS,'pointscatterers');P = rmfield(P,'pointscatterers');end
+P.X = sprintf('%g_%g',10*P.ymin,10*P.ymax);
+P.Y = sprintf('%g_%g',10*P.xmin,10*P.xmax);
+P.Z = sprintf('%g_%g',-10*P.zmax,-10*P.zmin);
+P = rmfield(P,{'xmin','xmax','ymin','ymax','zmin','zmax'});
+
+PHANTOM_DIR=fullfile(DEST_DIR,make_file_name('phantom',P));
+regeneratephantom = 0;
+d = dir(fullfile(PHANTOM_DIR,'phantom*.mat'));
+RF_DIR=fullfile(DEST_DIR,make_file_name('rf',P));
+RF_FILE=fullfile(RF_DIR,'rf');
+if isempty(d) || regeneratephantom
+    mkdir(PHANTOM_DIR); %matlab 7
+    mkphantomfromdyna3symmetry(DYN_FILE,ZDISPFILE,fullfile(PHANTOM_DIR,'phantom'),PPARAMS);
+    %mkphantomwithflow(DYN_FILE,ZDISPFILE,fullfile(PHANTOM_DIR,'phantom'),PPARAMS);
+end
+mkdir(RF_DIR); %matlab 7
+
+%% ------------- GENERATE RF SCANS OF SCATTERER FIELDS -------------------
+P = rmfield(PARAMS,{'RXOFFSET','BEAM_ORIGIN_X','BEAM_ORIGIN_Y','BEAM_ANGLE_X','BEAM_ANGLE_Y','COMPUTATIONMETHOD'});
+P.X = sprintf('%g_%g_%g',1e3*P.XMIN,1e3*P.XSTEP,1e3*P.XMAX);
+P.Y = sprintf('%g_%g_%g',1e3*P.YMIN,1e3*P.YSTEP,1e3*P.YMAX);
+P.PHI = sprintf('%g_%g_%g',P.PHIMIN,P.PHISTEP,P.PHIMAX);
+P.THETA = sprintf('%g_%g_%g',P.THMIN,P.THSTEP,P.THMAX);
+P = rmfield(P,{'XMIN','XMAX','XSTEP','YMIN','YMAX','YSTEP','PHIMIN','PHIMAX','PHISTEP','THMIN','THSTEP','THMAX'});
+P.NO_BEAMS = sprintf('%g_%g',P.NO_BEAMS_X,P.NO_BEAMS_Y);
+P = rmfield(P,{'NO_BEAMS_X','NO_BEAMS_Y'});
+P.NPAR = sprintf('%g_%g',P.NO_PARALLEL(1),P.NO_PARALLEL(2));
+P.PSPACE = sprintf('%g_%g',P.PARALLEL_SPACING(1),P.PARALLEL_SPACING(2));
+P = rmfield(P,{'NO_PARALLEL','PARALLEL_SPACING'});
+P.APEX = sprintf('%g',1e3*P.APEX);
+P.TX_FOCUS = sprintf('%g',1e3*P.TX_FOCUS);
+P.TX_FREQ = sprintf('%g',1e-6*P.TX_FREQ);
+P.FS = sprintf('%g',1e-6*P.field_sample_freq);
+P = rmfield(P,'field_sample_freq');
+P.RX_FOCUS = sprintf('%g',1e3*P.RX_FOCUS);
+P.TX_F_NUM = sprintf('%g_%g',P.TX_F_NUM(1),P.TX_F_NUM(2));
+P.RX_F_NUM = sprintf('%g_%g',P.RX_F_NUM(1),P.RX_F_NUM(2));
+P.GRID = sprintf('%g_%g_%g',1e3*P.GRIDSPACING(1),1e3*P.GRIDSPACING(2),1e3*P.GRIDSPACING(3));
+P = rmfield(P,'GRIDSPACING');
+
+do_dyna_scans(PHANTOM_DIR,RF_FILE,PARAMS);
+
+%% TRACK RF
+%load all RF into a big matrix
+n=1;
+while ~isempty(dir(sprintf('%s%03d.mat',RF_FILE,n)))
+   load(sprintf('%s%03d.mat',RF_FILE,n));
+   % some rf*.mat files are off by one axial index; allowing for some PARAMS.YSTEPnamic
+   % assignment of bigRF matrix
+   % bigRF(:,:,n)=rf;
+   bigRF(1:size(rf,1),:,n)=rf;
+   n=n+1;
+end;
+
+%% track the displacements
+[arfidata,correlation,axial]=estimate_disp(double(bigRF),PARAMS,TRACKPARAMS);
+
+%% save res_tracksim.mat (same format as experimental res*.mat files)
+TRACKEDFILENAME = fullfile(RF_DIR,['res_tracksim_' num2str(TRACKPARAMS.WAVELENGTHS) 'lambda.mat']);
+save(TRACKEDFILENAME,'arfidata','correlation','axial','PARAMS','TRACKPARAMS');
