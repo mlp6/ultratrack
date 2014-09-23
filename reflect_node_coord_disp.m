59feef11d2112ea05689aee975d5909aba2c5ec2
Mark Palmeri <mlp6@duke.edu>
2014-09-22 22:09:42 -0400

    can now accomodate coordinates and displacements

--- reflect_node_coord_disp.m
+++ reflect_node_coord_disp.m
@@ -1,10 +1,11 @@
-function [Xr, Yr, Zr] = reflect_node_coord(sym, X, Y, Z, debug);
-% function [Xr, Yr, Zr] = reflect_node_coord(sym, X, Y, Z, debug);
+function [Xr, Yr, Zr] = reflect_node_coord_disp(dtype, sym, X, Y, Z, debug);
+% function [Xr, Yr, Zr] = reflect_node_coord_disp(dtype, sym, X, Y, Z, debug);
 %
 % reflect quarter and half symmetry mesh node coordinates for full volume
 % scatterer simulations, assuming x = y = 0 at symmetry planes
 %
-% INPUTS:   sym (string) - 'q' (quarter), 'h' (half) symmetry
+% INPUTS:   dtype (string) - 'coord' or 'disp'; coordinate or displacement data 
+%           sym (string) - 'q' (quarter), 'h' (half) symmetry
 %           X (3D float) - node X coordinates
 %           Y (3D float) - node Y coordinates
 %           Z (3D float) - node Z coordinates
@@ -29,23 +30,30 @@ if debug == 1,
     plot3(Xorig, Yorig, Zorig, 'ko');
 end
 
+switch dtype
+    case 'coord'
+        inv_sign = -1;
+    case 'disp'
+        inv_sign = 1;
+end
+
 switch sym
     case 'h'
         % flip in elevation (x, dim 1)
         % x should go neg -> 0, so leave out last entry
-        Xr = cat(1, X, -flipdim(X(1:end-1,:,:), 1));
+        Xr = cat(1, X, inv_sgn*flipdim(X(1:end-1,:,:), 1));
         Yr = cat(1, Y, Y(1:end-1,:,:));
         Zr = cat(1, Z, Z(1:end-1,:,:));
     case 'q'
         % flip in elevation first (just like 'h')
-        Xr = cat(1, X, -flipdim(X(1:end-1,:,:), 1));
+        Xr = cat(1, X, inv_sign*flipdim(X(1:end-1,:,:), 1));
         Yr = cat(1, Y, Y(1:end-1,:,:));
         Zr = cat(1, Z, Z(1:end-1,:,:));
 
         % now flip laterally (y, dim 2)
         % y should go 0 -> pos, so leave out first entry
         Xr = cat(2, flipdim(Xr(:,2:end,:), 2), Xr);
-        Yr = cat(2, -flipdim(Yr(:,2:end,:), 2), Yr);
+        Yr = cat(2, inv_sign*flipdim(Yr(:,2:end,:), 2), Yr);
         Zr = cat(2, flipdim(Zr(:,2:end,:),2), Zr);
 end
 
